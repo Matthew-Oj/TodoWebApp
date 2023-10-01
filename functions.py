12a00d7d71efd64a0c17e1cda62d30cb8b2ca3bd
Matthew <110884385+Matthew-Oj@users.noreply.github.com>
2023-10-01 17:14:07 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -18,10 +18,3 @@ def write_todos(todos_arg, filepath=FILEPATH):
     """ Writes a to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
-
-
-# do not need to return anything as it is a file modification
-
-
-if __name__ == "__main__":
-    print("Test from fun")
